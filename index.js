5fe8c2d19b255fec46ee0136dfbf6d5015c0d5a6
Marcel Wiessler <marcel@gaisterhand.de>
2025-04-02 15:37:02 +0200

    fix action

--- index.js
+++ index.js
@@ -2,17 +2,44 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import * as exec from '@actions/exec';
 
-try {
+async function run() {
+    try {
+        const token = core.getInput('token');
+        const dir = core.getInput('dir') || ".";
+        const name = core.getInput('name');
 
-    const token = core.getInput('token');
-    const dir = core.getInput('dir') || ".";
-    const name = core.getInput('name');
-    
-    const cmd = `npx --yes needle-cloud deploy "${dir}" --name "${name}" --token "${token}"`;
-    const res = await exec.exec(cmd);
-    core.setOutput("url", res);
+        let output = '';
+        let error = '';
+        const options = {
+            listeners: {
+                stdout: (data) => {
+                    output += data.toString();
+                },
+                stderr: (data) => {
+                    error += data.toString();
+                }
+            }
+        };
 
+        const cmd = `npx --yes needle-cloud deploy "${dir}" --name "${name}" --token "${token}"`;
+        const exitCode = await exec.exec(cmd, [], options);
+        if (exitCode !== 0) {
+            throw new Error(`Command failed with exit code ${exitCode}: ${error}`);
+        }
+
+        // Extract URL from the output using regex
+        const urlMatch = output.match(/(https:\/\/[\w.-]+\.\w+[\w/.-]*)/);
+        if (urlMatch && urlMatch[0]) {
+            const deployUrl = urlMatch[0];
+            console.log(`Deployment URL: ${deployUrl}`);
+            core.setOutput("url", deployUrl);
+        } else {
+            core.warning("Could not find deployment URL in output");
+            core.setOutput("url", "");
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
-catch (error) {
-    core.setFailed(error.message);
-}
+
+run();
